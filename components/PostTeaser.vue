8c4c3869663cd970809223bb9705e464ca04c34f
nickypangers <nixonpang98@gmail.com>
2021-12-30 02:11:56 +0800

    added post teaser component

---
+++ components/PostTeaser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="rounded-xl border p-3">
+    <p>{{ post._id }}</p>
+    <p>{{ post.username }}</p>
+    <p class="text-lg">{{ post.title }}</p>
+    <p>{{ post.description }}</p>
+    <p>{{ post.createdAt }}</p>
+    <button @click="toggleCommentBox()">
+      <p>comments: {{ post.comments.length }}</p>
+    </button>
+    <form>
+      <input type="text" v-model="comment" placeholder="enter comment here" />
+      <button @click="addComment()">send</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PostTeaser',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isCommentBoxOpen: false,
+      comment: '',
+    }
+  },
+  computed: {
+    isLoggedIn: function () {
+      return this.$store.state.user.isLoggedIn
+    },
+  },
+  methods: {
+    toggleCommentBox: function () {
+      this.isCommentBoxOpen = !this.isCommentBoxOpen
+    },
+    addComment: function () {},
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <p>user: {{ user }}</p>
-    <p>isLoggedIn: {{ $store.state.user.isLoggedIn }}</p>
+  <div class="grid grid-cols-1 gap-3">
+    <post-teaser v-for="post in posts" :key="post._id" :post="post" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      posts: [],
+    }
+  },
   computed: {
     user() {
       return this.$store.state.user.user
+    },
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  methods: {
+    fetchPosts: async function () {
+      const response = await this.$axios.get('/posts')
+      const data = response.data
+      if (!data.success) {
+        console.debug('fetchPost', data.message)
+        return
+      }
+      this.posts = data.posts
     },
   },
 }
